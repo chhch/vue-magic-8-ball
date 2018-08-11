14c2e619d59b5e9b7c85b78c88e322b2d0f5da20
chhch <chhch+github@protonmail.com>
2018-08-11 12:31:36 +0200

    Refactor code

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ var app = new Vue({
         question: '',
         answer: 'I cannot give you an answer until you ask a question!',
         image: '',
-        type: 'neutral',
-        position: {
-            affirmative: '0',
-            contrary: '0',
-            neutral: '0'
+        tag: {
+            affirmative: 'yes',
+            contrary: 'no',
+            neutral: 'don\'t know'
         }
     },
     watch: {
@@ -28,24 +27,25 @@ var app = new Vue({
                 return
             }
             this.answer = 'Thinking...';
+
             var vm = this;
             axios.get('https://8ball.delegator.com/magic/JSON/question?')
                 .then(function (response) {
-                    console.log(response);
-                    vm.answer = _.capitalize(response.data.magic.answer);
-                    vm.type = response.data.magic.type.toLowerCase();
+                    vm.answer = response.data.magic.answer;
 
-                    return axios.get('https://api.tenor.com/v1/search?q=' + vm.type + '&media_filter=minimal&limit=1&pos=' + vm.position[vm.type]);
-                    // return axios.get('https://api.tenor.com/v1/random?q=' + vm.type + '&media_filter=minimal&limit=1&pos=' + vm.position[vm.type]);
+                    const type = response.data.magic.type.toLowerCase();
+                    const params = {
+                        q: vm.tag[type],
+                        media_filter: 'minimal',
+                        limit: '1'
+                    };
+                    return axios.get('https://api.tenor.com/v1/random', {params: params });
 
                 })
                 .then(function (response) {
-                    console.log(response);
                     vm.image = response.data.results[0].media[0].gif.url;
-                    vm.position[vm.type] = response.data.next ? response.data.next : '0';
                 })
                 .catch(function (error) {
-                    console.log(error);
                     vm.answer = 'Error! Could not reach the API. ' + error
                 })
         }
